9d9d3941ae0ba4d922fc014b0084c311ddbbff8d
Faintful <72162133+Faintful@users.noreply.github.com>
2024-02-03 21:25:26 +0100

    @PathVariable

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,7 +5,9 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,18 +16,19 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer") // Equivalent to: @RequestMapping(path = "/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         log.debug("Get a List of Beers - In Controller");
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID uuid) {
-
+    @RequestMapping(method = RequestMethod.GET, path = "/{beerId}")
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
         log.debug("Get Beer By ID - In Controller");
-        return beerService.getBeerByID(uuid);
+        return beerService.getBeerByID(beerId);
     }
 }
