1de16256ecb884183fef7fcdfbad112e8af26783
Faintful <72162133+Faintful@users.noreply.github.com>
2024-02-21 20:40:58 +0100

    Corrected a naming mistake in the Customer mapper

--- src/main/java/guru/springframework/spring6restmvc/mappers/CustomerMapper.java
+++ src/main/java/guru/springframework/spring6restmvc/mappers/CustomerMapper.java
@@ -1,12 +1,12 @@
 package guru.springframework.spring6restmvc.mappers;
 
-import guru.springframework.spring6restmvc.entities.Beer;
-import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface CustomerMapper {
-    Beer beerDtoToBeer(BeerDTO dto);
+    Customer customerDtoToCustomer(CustomerDTO dto);
 
-    BeerDTO beerToBeerDto(Beer beer);
+    CustomerDTO customerToCustomerDto(Customer customer);
 }
