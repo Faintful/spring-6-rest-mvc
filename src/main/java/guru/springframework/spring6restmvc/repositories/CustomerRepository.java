5398788d1a0af956198fdaca54859e264f06b672
Faintful <72162133+Faintful@users.noreply.github.com>
2024-02-21 20:50:35 +0100

    Corrrected another naming mistake this time in CustomerRepository

--- src/main/java/guru/springframework/spring6restmvc/repositories/CustomerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/CustomerRepository.java
@@ -1,9 +1,9 @@
 package guru.springframework.spring6restmvc.repositories;
 
-import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CustomerRepository extends JpaRepository<Beer, UUID> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 }
