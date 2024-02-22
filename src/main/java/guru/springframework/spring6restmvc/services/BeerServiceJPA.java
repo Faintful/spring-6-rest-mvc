8e48e4f070e0c8ff04b8bdd44629a74331b84e6a
Faintful <72162133+Faintful@users.noreply.github.com>
2024-02-22 17:55:14 +0100

    Created primary implementations of BeerService and CustomerService that use JPA

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.mappers.BeerMapper;
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService{
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerByID(UUID uuid) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateBeer(UUID uuid, BeerDTO beerDTO) {
+
+    }
+
+    @Override
+    public void deleteById(UUID uuid) {
+
+    }
+
+    @Override
+    public Optional<BeerDTO> patchBeer(UUID uuid, BeerDTO beerDTO) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO getCustomerById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateById(UUID uuid, CustomerDTO customerDTO) {
+
+    }
+}
