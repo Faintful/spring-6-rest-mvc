026fc54e6a74e22596f6d1203d94998fb22f62b7
Faintful <72162133+Faintful@users.noreply.github.com>
2024-02-23 19:56:20 +0100

    Added Unhappy Path test for listCustomer() in CustomerControllerIT

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -4,8 +4,12 @@ import guru.springframework.spring6restmvc.entities.Customer;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import guru.springframework.spring6restmvc.services.CustomerService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CustomerControllerIT {
 
     @Autowired
@@ -31,10 +36,18 @@ class CustomerControllerIT {
         customerDTOS.forEach((customerDTO) -> {
             log.info(customerDTO.toString());
         });
+        assertThat(customerDTOS.size()).isEqualTo(3);
+    }
+
+    @Transactional
+    @Test
+    @Order(1)
+    void listCustomerUP() {
         List<Customer> allCustomers = customerRepository.findAll();
         allCustomers.forEach((customer) -> {
             log.info(customer.toString());
         });
-        assertThat(customerDTOS.size()).isEqualTo(allCustomers.size());
+        customerRepository.deleteAll();
+        assertThat(customerRepository.count()).isEqualTo(0);
     }
 }
